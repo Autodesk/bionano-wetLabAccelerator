5e2edccdf84a9714aba1b36033953130d3173799
Maxwell Bates <maxbates@gmail.com>
2015-02-23 17:11:37 -0800

    autogrow directive for protocol name

---
+++ app/scripts/directives/autogrow.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name transcripticApp.directive:autogrow
+ * @description
+ * # autogrow
+ */
+angular.module('transcripticApp')
+  .directive('autoGrow', function($timeout) {
+    return {
+      require: '?ngModel',
+      link: function(scope, element, attr, ngModelCtrl){
+        var paddingLeft = element.css('paddingLeft'),
+            paddingRight = element.css('paddingRight');
+
+        var minWidth = parseInt(attr.autoGrow, 10) || parseInt(element.css('minWidth'), 10) || 100;
+
+        console.log(minWidth);
+
+        var $shadow = angular.element('<span></span>').css({
+          'position': 'absolute',
+          'top': '-10000px',
+          'left': '-10000px',
+          'fontSize': element.css('fontSize'),
+          'fontFamily': element.css('fontFamily'),
+          'white-space': 'pre'
+        });
+        element.after($shadow);
+
+        var update = function() {
+          var val = element.val()
+                .replace(/</g, '&lt;')
+                .replace(/>/g, '&gt;')
+                .replace(/&/g, '&amp;')
+            ;
+
+          // If empty calculate by placeholder
+          if (val !== "") {
+            $shadow.html(val);
+          } else {
+            $shadow.html(element[0].placeholder);
+          }
+
+          //extra to handle new letter before next $digest
+          var calcWidth = $shadow[0].offsetWidth + 26;
+          var newWidth = Math.max(calcWidth, minWidth) + "px";
+          element.css('width', newWidth);
+        };
+
+        if (ngModelCtrl) {
+          scope.$watch(function () {
+            return ngModelCtrl.$viewValue;
+          }, update)
+        } else {
+          element.bind('keyup keydown blur', update);
+        }
+
+        // Update on the first link
+        // $timeout is needed because the value of element is updated only after the $digest cycle
+        // TODO: Maybe on compile time if we call update we won't need $timeout
+        $timeout(update);
+      }
+    }
+  });
