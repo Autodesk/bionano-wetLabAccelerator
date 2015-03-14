e0c3ce961999668394a88475804a1765007e7325
Maxwell Bates <maxbates@gmail.com>
2015-03-13 21:33:01 -0700

    work on protocol field directive, add basic mixwrap support + others
    address #20

--- app/scripts/directives/tx-protocol-field.js
+++ app/scripts/directives/tx-protocol-field.js
@@ -18,41 +18,60 @@ angular.module('transcripticApp')
       bindToController: true,
       controllerAs: 'fieldCtrl',
       controller: function ($scope, $element, $attrs) {
+        var self = this;
 
+        self.mixwrapToggle = function (newval) {
+          //todo - handle mixwrap hidden / not hidden -- don't want to include in Autoprotocol if hidden
+        }
       },
       compile: function compile(tElement, tAttrs, transclude) {
         return {
           pre: function preLink(scope, iElement, iAttrs) {
             var type = scope.fieldCtrl.field.type,
                 inputType = InputTypes[type],
-                partial;
+                partial = type;                 //default, maybe handled differently in if/else
 
-            console.log(type);
+            //console.log(type);
 
+            //handle all dimensional values the same way
             if (inputType['autoprotocol-type'] == 'Unit') {
               partial = 'dimension';
               scope.unitOptions = inputType.units;
-              var split = scope.fieldCtrl.model.split(':');
-              //todo - $watch
+
+              handleNewDimensionalExternal(scope.fieldCtrl.model);
+              scope.$watch('internal', handleNewDimensionInternal, true);
+              scope.$watch('fieldCtrl.model', handleNewDimensionalExternal);
+            }
+            else if (type == 'option') {
+              scope.modelOptions = scope.fieldCtrl.field.options;
+            }
+
+            /* functions for specific types */
+
+            function handleNewDimensionInternal (newobj) {
+              //todo - assign using ngModelCtrl (need to do in postlink) - this works but not best way?
+              if (_.isEmpty(newobj)) { return; }
+              scope.fieldCtrl.model = '' + newobj.value + ':' + newobj.unit;
+            }
+
+            function handleNewDimensionalExternal (newval) {
+              if (!_.isString(newval)) { return; }
+              var split = newval.split(':');
               scope.internal = {
                 value: parseInt(split[0], 10),
                 unit: split[1]
               };
             }
-            else if (type == 'option') {
-              partial = 'option';
-              scope.modelOptions = scope.fieldCtrl.field.options;
-            }
-            else {
-              partial = type;
-            }
+
+            /* get the partial */
 
             return $http.get('views/inputs/' + partial + '.html').then(function (data) {
               iElement.html($compile(angular.element(data.data))(scope));
+              //todo - add inputAttrs e.g. for dimensional
             });
           },
           post: function postLink(scope, iElement, iAttrs, ngModel) {
-            console.log('post_link');
+
           }
         }
       }
