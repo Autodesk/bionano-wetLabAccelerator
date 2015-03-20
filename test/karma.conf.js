7ed0f42c8a484ecac87f96494f70d6426115e0ea
Maxwell Bates <maxbates@gmail.com>
2015-03-20 13:03:58 -0700

    get test suite working (none of the tests pass yet though)

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -27,6 +27,10 @@ module.exports = function(config) {
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
+      'bower_components/angularfire/dist/angularfire.js',
+      'bower_components/angular-bootstrap/ui-bootstrap.js',
+      'bower_components/angular-ui-sortable/sortable.js',
+      'bower_components/ng-file-upload/angular-file-upload.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
