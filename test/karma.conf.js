be4506eb72ec19324cf84fc55bded7fb670c067f
Maxwell Bates <maxbates@gmail.com>
2015-04-15 14:43:56 -0700

    adds basic scaffolding for testing, need example protocols, example communication with transcriptic (verify proxy working)

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -71,10 +71,11 @@ module.exports = function(config) {
     logLevel: config.LOG_INFO,
 
     // Uncomment the following lines if you are using grunt's server to run the tests
-    // proxies: {
-    //   '/': 'http://localhost:9000/'
-    // },
-    // URL root prevent conflicts with the site root
-    // urlRoot: '_karma_'
+    proxies: {
+      '/': 'http://localhost:9000/'
+    },
+
+    //URL root prevent conflicts with the site root
+    urlRoot: '_karma_'
   });
 };
