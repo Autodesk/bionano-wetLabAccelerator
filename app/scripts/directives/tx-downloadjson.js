96e463589740cd39a05bff9404953fa3f12d8577
Maxwell Bates <maxbates@gmail.com>
2015-04-14 17:04:04 -0700

    can download autoprotocol and omniprotocol from editor
    close #19

--- app/scripts/directives/tx-downloadjson.js
+++ app/scripts/directives/tx-downloadjson.js
@@ -6,33 +6,44 @@
  * @description
  * # txDownloadjson
  */
-//fixme - giving maximum callstack error ??
 angular.module('transcripticApp')
-  .directive('txDownloadjson', function ($window) {
+  .directive('txDownloadjson', function ($window, $document) {
     return {
       restrict: 'A',
       scope: {
         filename: '@',
         downloadModel: '=',
-        downloadEvent: '@'
+        downloadEvent: '@',
+        downloadFormatter: '='
       },
       link: function postLink(scope, element, attrs) {
         element.on(scope.downloadEvent || 'click', function (e) {
+          console.log('clicked', e.target);
+
+          //only needed when appending to element, not body
+          //handle clicking the a child, and prevent propagation + infinite loop
+          if (e.target != element[0]) {
+            e.stopPropagation();
+            return;
+          }
+
           var a = document.createElement("a"),
-            blob = new Blob([angular.toJson(scope.downloadModel, true)], {type: "application/json"}),
-            url = $window.URL.createObjectURL(blob);
+              formatter = _.isFunction(scope.downloadFormatter) ? scope.downloadFormatter : _.identity,
+              formatted = formatter(scope.downloadModel),
+              blob = new Blob([angular.toJson(formatted, true)], {type: "application/json"}),
+              url = $window.URL.createObjectURL(blob);
 
           a.style = "display: none";
           a.href = url;
           a.download = scope.filename || 'saved_json.json';
 
-          element.append(a);
-          a.click();
+          $document[0].body.appendChild(a);
 
-          $window.URL.revokeObjectURL(url);
-          element[0].removeChild(a);
-
-          e.preventDefault();
+          setTimeout(function () {
+            a.click();
+            $window.URL.revokeObjectURL(url);
+            $document[0].body.removeChild(a);
+          }, 50);
         });
       }
     };
