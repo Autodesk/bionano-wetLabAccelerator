1770265c1d216afde0a74e79c18bed149918f86b
Maxwell Bates <max.bates@autodesk.com>
2015-04-30 16:02:32 -0700

    better run creation handling for transcriptic IDs

--- app/scripts/services/runhelper.js
+++ app/scripts/services/runhelper.js
@@ -8,36 +8,42 @@
  * Service in the transcripticApp.
  */
 angular.module('transcripticApp')
-  .service('RunHelper', function ($q, simpleLogin, FBProfile, Omniprotocol) {
+  .service('RunHelper', function ($q, Authentication, Run, ProtocolHelper, simpleLogin, FBProfile, Omniprotocol, UUIDGen) {
 
     var self = this;
 
     self.runs = [];
     self.currentRun = {};
 
-    self.createRun = function (protocol) {
+    self.createRun = function (protocol, transcripticProject, testMode) {
       var run = _.assign(Omniprotocol.utils.getScaffoldRun(), {
-        parameters : _.cloneDeep(protocol.parameters)
+        protocol : _.cloneDeep(protocol),
+        autoprotocol : ProtocolHelper.convertToAutoprotocol(protocol)
       });
 
       //assign metadata
-      _.assign(run.metadata, {
-        date : (new Date()).valueOf(),
-        protocol : {
-          name : protocol.metadata.name,
-          id : protocol.metadata.id,
-          author: "todo"
-        },
-        author : {
-          'todo' : 'todo'
-        }
-      });
+      _.assign(run.metadata, generateNewRunMetadata());
 
-      //todo - run it and get an id from transcriptic
+      //todo - verify
+
+      return Run.submit({project: transcripticProject}, {
+        title: 'Run of ' + protocol.metadata.name,
+        protocol: run.autoprotocol,
+        test_mode: !!testMode
+      }).$promise.then(function (submissionResult) {
+          console.log(submissionResult);
+
+          run.transcripticRunId = submissionResult.id;
+
+          return self.firebaseRuns.$add(run)
+            .then(updateRunsExposed);
+      }, function (submissionFailure) {
+          console.warn('run failure', submissionFailure);
+      });
     };
 
     self.assignCurrentRun = function (inputRun) {
-      _.assign(self.currentRun, Omniprotocol.utils.getScaffoldRun(), inputRun)
+      _.assign(self.currentRun, Omniprotocol.utils.getScaffoldRun(), inputRun);
     };
 
     // watchers //
@@ -51,6 +57,21 @@ angular.module('transcripticApp')
     });
 
     // helpers //
+
+    //todo - handle tags
+    //note - does not handle protocol
+    function generateNewRunMetadata () {
+      return {
+        id  : UUIDGen(),
+        date: '' + (new Date()).valueOf(),
+        type: 'run',
+        author : {
+          name : Authentication.getUsername(),
+          id : Authentication.getUserId()
+        },
+        protocol: {}
+      }
+    }
 
     function setRunList (runs) {
       self.runs.length = 0;
