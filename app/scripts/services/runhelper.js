42065f684db1d6351e7986144e7e5072cce0ee6c
Maxwell Bates <max.bates@autodesk.com>
2015-05-04 21:49:07 -0700

    update with data until complete, add functions to RunHelper (need to verify)

--- app/scripts/services/runhelper.js
+++ app/scripts/services/runhelper.js
@@ -12,15 +12,19 @@ angular.module('transcripticApp')
 
     var self = this;
 
-    self.runs = [];
+    self.runs       = [];
     self.currentRun = {};
+
+    self.assignCurrentRun = function (inputRun) {
+      return _.assign(self.currentRun, Omniprotocol.utils.getScaffoldRun(), inputRun);
+    };
 
     self.verifyRun = function (protocol, transcripticProject, testMode) {
       var run = createNewRunObject(protocol);
 
       return Run.analyze({project: transcripticProject}, {
-        title: 'Verification of ' + protocol.metadata.name + ' - ' + Date.now(),
-        protocol: run.autoprotocol,
+        title    : 'Verification of ' + protocol.metadata.name + ' - ' + Date.now(),
+        protocol : run.autoprotocol,
         test_mode: !!testMode
       }).$promise
     };
@@ -29,29 +33,75 @@ angular.module('transcripticApp')
       var run = createNewRunObject(protocol);
 
       return Run.submit({project: transcripticProject}, {
-        title: 'Run of ' + protocol.metadata.name,
-        protocol: run.autoprotocol,
+        title    : 'Run of ' + protocol.metadata.name,
+        protocol : run.autoprotocol,
         test_mode: !!testMode
       }).$promise.then(function (submissionResult) {
           console.log(submissionResult);
 
           _.assign(run, {
-            transcripticRunId : submissionResult.id,
-            transcripticRunInfo : _.cloneDeep(submissionResult)
+            transcripticProjectId: transcripticProject,
+            transcripticRunId    : submissionResult.id,
+            transcripticRunInfo  : _.cloneDeep(submissionResult)
           });
 
+          //note - firebase
           return self.firebaseRuns.$add(run)
             .then(updateRunsExposed)
             .then(_.partial($q.when, submissionResult));
 
-      }, function (submissionFailure) {
+        }, function (submissionFailure) {
           console.warn('run failure', submissionFailure);
           return $q.reject(submissionFailure);
-      });
+        });
     };
 
-    self.assignCurrentRun = function (inputRun) {
-      _.assign(self.currentRun, Omniprotocol.utils.getScaffoldRun(), inputRun);
+    self.updateRunInfo = function (runObj) {
+      var runId       = _.result(runObj, 'transcripticRunId'),
+          projectId   = _.result(runObj, 'transcripticProjectId'),
+          runInfo     = _.result(runObj, 'transcripticRunInfo'),
+          runStatus   = _.result(runInfo, 'status', ''),
+          runCompleted = (runStatus == 'complete');
+
+      if ((_.isUndefined(runInfo) || !runCompleted) && runId && projectId) {
+        return Run.view({project: projectId, run: runId})
+          .$promise
+          .then(function updateRunInfoSuccess (runInfo) {
+            return _.assign(runObj, {
+              transcripticRunInfo: runInfo
+            });
+          })
+          .then(self.saveRun);
+      } else {
+        if (!runId || !projectId) {
+          console.warn('run information for transcriptic is missing...')
+        }
+        return $q.when(runObj);
+      }
+    };
+
+    self.saveRun = function (runObj) {
+      if (!hasNecessaryMetadataToSave(runObj)) {
+        assignNecessaryMetadataToRun(runObj);
+      }
+
+      //hack for firebase
+      //todo - verify this is working
+      var firebaseRecord = self.firebaseRuns.$getRecord(runObj.$id);
+      if (runObj.$id && firebaseRecord) {
+        //console.log(firebaseRecord);
+        _.assign(firebaseRecord, runObj);
+        return self.firebaseRuns.$save(firebaseRecord)
+          .then(updateRunsExposed);
+      } else {
+        return self.firebaseRuns.$add(runObj).
+          then(function (ref) {
+            var firebaseProto = self.firebaseRuns.$getRecord(ref.key());
+            //console.log(ref.key(), firebaseProto);
+            !_.isEmpty(firebaseProto) && self.assignCurrentRun(firebaseProto);
+          })
+          .then(updateRunsExposed);
+      }
     };
 
     // watchers //
@@ -71,8 +121,8 @@ angular.module('transcripticApp')
 
     function createNewRunObject (protocol) {
       var run = _.assign(Omniprotocol.utils.getScaffoldRun(), {
-        protocol : _.cloneDeep(protocol),
-        autoprotocol : ProtocolHelper.convertToAutoprotocol(protocol)
+        protocol    : _.cloneDeep(protocol),
+        autoprotocol: ProtocolHelper.convertToAutoprotocol(protocol)
       });
 
       //assign metadata
@@ -82,18 +132,33 @@ angular.module('transcripticApp')
     }
 
     //todo - handle tags
-    //note - does not handle protocol
+    //note - does not handle protocol, need to attach that separately (not in metadata)
     function generateNewRunMetadata (protocol) {
       return {
-        id  : UUIDGen(),
-        name: 'Run of ' + _.result(protocol.metadata, 'name', 'CX1 Protocol'),
-        date: (new Date()).toString(),
-        type: 'run',
-        author : {
-          name : Authentication.getUsername(),
-          id : Authentication.getUserId()
+        id      : UUIDGen(),
+        name    : 'Run of ' + _.result(protocol.metadata, 'name', 'CX1 Protocol'),
+        date    : (new Date()).toString(),
+        type    : 'run',
+        author  : {
+          name: Authentication.getUsername(),
+          id  : Authentication.getUserId()
+        },
+        protocol: {
+          id    : protocol.metadata.id,
+          name  : protocol.metadata.name,
+          author: protocol.metadata.author
         }
       }
+    }
+
+    function assignNecessaryMetadataToRun (runObj) {
+      return _.assign(runObj.metadata, generateNewRunMetadata(), runObj.metadata);
+    }
+
+    function hasNecessaryMetadataToSave (runObj) {
+      return _.every(['id', 'name', 'type', 'author', 'protocol'], function (field) {
+        return !_.isUndefined(_.result(runObj.metadata, field));
+      });
     }
 
     function setRunList (runs) {
