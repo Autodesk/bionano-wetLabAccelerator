3bd51544db0c4d62a0c1cc94212a54d0df4666d6
Maxwell Bates <max.bates@autodesk.com>
2015-05-07 13:15:14 -0700

    can add groups, hide old UI, defaults colors

--- app/scripts/controllers/columnvolumes.js
+++ app/scripts/controllers/columnvolumes.js
@@ -11,10 +11,33 @@ angular.module('transcripticApp')
   .controller('columnVolumesCtrl', function ($scope) {
     var self = this;
 
+    //todo - support more colors
+    self.colors = [
+      'red',
+      'blue',
+      'green',
+      'yellow',
+      'magenta',
+      'cyan',
+      'white'
+    ];
+
     self.colvolActive = 0;
 
     self.activateGroup = function (index) {
       self.colvolActive = index;
+    };
+
+    //todo - prevent adding too many empty groups
+    self.addColumn = function () {
+      self.groups.push({
+        columns: [],
+        volume: {
+          value: 50,
+          unit: 'microliter'
+        }
+      });
+      self.assignColors();
     };
 
     self.handleColumnSelection = function (column) {
@@ -40,4 +63,10 @@ angular.module('transcripticApp')
         self.groups[self.colvolActive].columns.push('' + column);
       }
     };
+
+    self.assignColors = function() {
+      _.forEach(self.groups, function (group, index) {
+        _.assign(group, {color: self.colors[index]})
+      });
+    }
   });
