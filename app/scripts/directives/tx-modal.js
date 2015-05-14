ac6f25c2e1dc982dce9a4c0543e5fc833d56cd93
Maxwell Bates <max.bates@autodesk.com>
2015-05-14 11:42:16 -0700

    autovirology-353 modal - add $close and $onClose to transcluded scope
     close #353

--- app/scripts/directives/tx-modal.js
+++ app/scripts/directives/tx-modal.js
@@ -8,28 +8,43 @@
  */
 angular.module('transcripticApp')
   .directive('txModal', function () {
-      return {
-        templateUrl: 'views/tx-modal.html',
-        restrict: 'E',
-        scope: {
-          ngShow: '=',
-          title: '@?',
-          onClose: '&?'
-        },
-        transclude: true,
-        link: function(scope, element, attrs) {
-          scope.dialogStyle = {};
-          if (attrs.top) {
-            scope.dialogStyle.top = attrs.top;
-          }
-          if (attrs.height) {
-            scope.dialogStyle.height = attrs.height;
-          }
-          scope.hideModal = function() {
-            angular.isFunction(scope.onClose) && scope.onClose();
-            scope.ngShow = false;
-          };
+    return {
+      templateUrl: 'views/tx-modal.html',
+      restrict   : 'E',
+      scope      : {
+        ngShow : '=',
+        title  : '@?',
+        onClose: '&?'
+      },
+      transclude : true,
+      link       : function (scope, element, attrs, ctrl, transcludeFn) {
+
+        scope.dialogStyle = {};
+        if (attrs.top) {
+          scope.dialogStyle.top = attrs.top;
         }
-      };
+        if (attrs.height) {
+          scope.dialogStyle.height = attrs.height;
+        }
+
+        scope.hideModal = function () {
+          angular.isFunction(scope.onClose) && scope.onClose();
+          scope.ngShow = false;
+
+          transcludeFn(function (clone, scope) {
+            console.log(scope);
+            _.attempt(scope.$onClose);
+          });
+        };
+
+        //can't use ng-transclude and update the scope, so add the element manually
+        transcludeFn(function (clone, transcludeScope) {
+          var transcludeEl       = element.find('modal-transclude');
+          transcludeEl.empty();
+          transcludeEl.append(clone);
+          transcludeScope.$close = scope.hideModal;
+        });
+      }
+    };
 
   });
