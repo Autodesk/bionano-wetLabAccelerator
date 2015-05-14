4f3b58a3bec32eb092cdb4e5debf9f60607f7ae6
Maxwell Bates <max.bates@autodesk.com>
2015-05-14 13:49:12 -0700

    basic scaffolding for operation summaries
    address #125

---
+++ app/scripts/directives/tx-operation-popover.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name transcripticApp.directive:txOperationPopover
+ * @description
+ * # txOperationPopover
+ */
+angular.module('transcripticApp')
+  .directive('txOperationPopover', function () {
+    return {
+      templateUrl: 'views/tx-operation-popover.html',
+      restrict: 'E',
+      scope: {
+        operation: '='
+      },
+      bindToController: true,
+      controllerAs: 'popCtrl',
+      controller: function ($scope, $element, $attrs) {
+
+      },
+      link: function postLink(scope, element, attrs) {
+
+      }
+    };
+  });
