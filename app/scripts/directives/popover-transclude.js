d8cea947c02e614a3f97bde866ee16b6d8b21ea2
Maxwell Bates <max.bates@autodesk.com>
2015-05-19 16:25:47 -0700

    popover on bottom if no room, click outside listener
    close #354

--- app/scripts/directives/popover-transclude.js
+++ app/scripts/directives/popover-transclude.js
@@ -7,57 +7,94 @@
  * # popoverTransclude
  */
 angular.module('transcripticApp')
-  .directive('popoverTransclude', function ($document, $position) {
+  .directive('popoverTransclude', function ($document, $timeout, $position) {
     return {
       restrict   : 'A',
       replace    : true,
       transclude : true,
       scope      : {
-        title: '@',
+        title : '@',
         isOpen: '='
       },
       templateUrl: 'views/popover-transclude.html',
       link       : function (scope, element, attrs) {
 
-        //todo - put on bottom if not oom (update styles)
-
-        //todo - make so can change
+        //future - make so can change
         var target = element.parent();
 
-        //todo- attach click outside listener, escape listener
-
-        $document.find('body').append(element);
-
-        positionPopover(target);
-
         scope.$close = function () {
-          scope.isOpen = false
+          scope.isOpen = false;
         };
 
-        scope.$on('$locationChangeSuccess', function closePopoverOnLocationChangeSuccess () {
+        scope.$on('$locationChangeSuccess', function popoverCloseOnLocation () {
           if (scope.isOpen) {
             scope.$close();
+          }
+          removePopover();
+        });
+
+        scope.$watch('isOpen', function (open) {
+          if (open) {
+            positionPopover(target);
+            $timeout(registerTriggers);
+          } else {
+            //'open' class will be handled in template by internalOpen
+            unregisterTriggers();
           }
         });
 
         // Make sure tooltip is destroyed and removed.
         scope.$on('$destroy', function onDestroyTooltip () {
           unregisterTriggers();
+          removePopover();
         });
 
+        //this relies on the popover being in the page... could add transforms to the popover and position before (e.g. for top translate -50%, 100%)
         function positionPopover (targetEl) {
-          //hack - expect top
-          var ttPosition = $position.positionElements(targetEl, element, 'top', true);
-          ttPosition.top = (ttPosition.top - 12) + 'px';
-          ttPosition.left += 'px';
+          scope.placement = 'top';
+          var pos         = $position.positionElements(targetEl, element, scope.placement || 'top', true),
+              arrowHeight = 12;
+
+          pos.top -= arrowHeight;
+
+          var elementHeight = _.parseInt(element.css('height'), 10);
+
+          //put on bottom if not room (and update styles)
+          if (elementHeight < 0) {
+            scope.placement = 'bottom';
+            pos             = $position.positionElements(targetEl, element, scope.placement, true);
+            pos.top += arrowHeight;
+          }
+
+          pos.top += 'px';
+          pos.left += 'px';
 
           // Now set the calculated positioning.
-          element.css(ttPosition);
+          element.css(pos);
+        }
+
+        function outsideClickListener ($event) {
+          if (element[0].contains($event.target)) {
+            //don't do anything....
+          } else {
+            scope.$apply(scope.$close);
+          }
+        }
+
+        function registerTriggers () {
+          $document.on('click', outsideClickListener);
         }
 
         function unregisterTriggers () {
-
+          $document.off('click', outsideClickListener);
         }
+
+        function removePopover () {
+          element.remove();
+        }
+
+        //init
+        $document.find('body').append(element);
       }
     };
   });
