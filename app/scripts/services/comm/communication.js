1bc72508a72c17ce0d67316668f4e38c3e5d9eb4
Maxwell Bates <max.bates@autodesk.com>
2015-06-02 13:57:20 -0700

    proper checking of credentials

--- app/scripts/services/comm/communication.js
+++ app/scripts/services/comm/communication.js
@@ -59,8 +59,7 @@ angular.module('tx.communication')
     //returns promise which will resolve with organizations if valid, fail if not
     this.validate = function validateCreds () {
 
-      var headers = TranscripticAuth.headers();
-      if (!_.every(['organization', 'email', 'key'], _.partial(_.has, headers, _))) {
+      if (!_.every(['organization', 'email', 'key'], _.partial(_.result, TranscripticAuth, _))) {
         return $q.reject(false);
       }
 
