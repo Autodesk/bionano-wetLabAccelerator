80b292dd60181152fb6cd5c34c8f73e2e412560d
Maxwell Bates <max.bates@autodesk.com>
2015-06-04 19:43:48 -0700

    add ng-show-auth and ng-hide-auth directives
    address #571

--- app/scripts/services/authentication.js
+++ app/scripts/services/authentication.js
@@ -94,4 +94,59 @@ angular.module('transcripticApp')
       self.authenticate(initialAuthToken);
     }
 
+  })
+/**
+ * A directive that shows elements only when user is logged in.
+ */
+  .directive('ngShowAuth', function (Authentication, $timeout) {
+    var isLoggedIn;
+    Authentication.watch(function(user) {
+      isLoggedIn = !!user;
+    });
+
+    return {
+      restrict: 'A',
+      link: function(scope, el) {
+        el.addClass('ng-cloak'); // hide until we process it
+
+        function update() {
+          // sometimes if ngCloak exists on same element, they argue, so make sure that
+          // this one always runs last for reliability
+          $timeout(function () {
+            el.toggleClass('ng-cloak', !isLoggedIn);
+          }, 0);
+        }
+
+        update();
+        Authentication.watch(update, scope);
+      }
+    };
+  })
+
+/**
+ * A directive that shows elements only when user is logged out.
+ */
+  .directive('ngHideAuth', function (Authentication, $timeout) {
+    var isLoggedIn;
+    Authentication.watch(function(user) {
+      isLoggedIn = !!user;
+    });
+
+    return {
+      restrict: 'A',
+      link: function(scope, el) {
+        function update() {
+          el.addClass('ng-cloak'); // hide until we process it
+
+          // sometimes if ngCloak exists on same element, they argue, so make sure that
+          // this one always runs last for reliability
+          $timeout(function () {
+            el.toggleClass('ng-cloak', isLoggedIn !== false);
+          }, 0);
+        }
+
+        update();
+        Authentication.watch(update, scope);
+      }
+    };
   });
